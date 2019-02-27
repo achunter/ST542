36e6d5d42609a81b6b936b9fb8b26089f5320fe4
achunter <48038975+achunter@users.noreply.github.com>
2019-02-27 07:13:47 -0500

    Add files via upload

--- server.R
+++ server.R
@@ -1,6 +1,22 @@
 library(shiny)
 
 shinyServer(function(input,output) {
+  
+  
+  #####################################################
+  # PRIOR TO RUNNING APP, PLEASE RUN THE FOLLOWING CODE
+  #####################################################
+  
+  # Load data
+  data.nc <- read.csv('ST542NC.csv')
+  data.de <- read.csv('ST542DE.csv')
+  #data.comb <- read.csv('ST542COMB.csv')
+  
+  
+  #################################################
+  # PRIOR TO RUNNING APP, PLEASE RUN THE ABOVE CODE
+  #################################################
+  
   output$CityComp <- renderPlot({
     plot(data.comb$city, data.comb[,input$field], 
          main=paste('Box Plot Comparison of', input$field, 'by City', sep=' '))
