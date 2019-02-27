8f830b09c333c236f5f2c85489277998ecc6fa07
achunter <48038975+achunter@users.noreply.github.com>
2019-02-27 07:15:47 -0500

    Add files via upload

--- server.R
+++ server.R
@@ -3,19 +3,31 @@ library(shiny)
 shinyServer(function(input,output) {
   
   
-  #####################################################
-  # PRIOR TO RUNNING APP, PLEASE RUN THE FOLLOWING CODE
-  #####################################################
-  
   # Load data
   data.nc <- read.csv('ST542NC.csv')
   data.de <- read.csv('ST542DE.csv')
   #data.comb <- read.csv('ST542COMB.csv')
   
+  names(data.nc) <- c('city', 'site', 'beetle_cnt', 'beetle_rich',
+                      'sw_index', 'nn_rich', 'nat_rich', 'nonnative_perc',
+                      'avg_nudds', 'avg_densio', 'litter', 'pH', 'K',
+                      'dist_from_city', 'diameter')
+  names(data.de) <- names(data.nc)
   
-  #################################################
-  # PRIOR TO RUNNING APP, PLEASE RUN THE ABOVE CODE
-  #################################################
+  # Combine Raleigh and Newark data into one data frame, then remove site variable
+  
+  data.comb <- rbind(data.nc, data.de)
+  data.comb <- data.comb[,-2]
+  
+  
+  # City and distance from city should be treated as factors, not numeric
+  
+  data.comb$city <- ifelse(data.comb$city == 1, 'Raleigh, NC','Newark, DE')
+  data.comb$city <- as.factor(data.comb$city)
+  
+  data.comb$dist_from_city <- as.factor(data.comb$dist_from_city)
+  
+  
   
   output$CityComp <- renderPlot({
     plot(data.comb$city, data.comb[,input$field], 
